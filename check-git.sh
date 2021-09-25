20e902a4b9c8ddd3d4234650f8ec658613ac3d8a
Angelo Reale <info@angeloreale.com>
2021-09-25 19:41:54 -0300

    better variable name

--- check-git.sh
+++ check-git.sh
@@ -1,5 +1,5 @@
 # change to your repo branch to publish
-basebranch="master"
+publishingbranch="master"
 
 
 # don't bother about what comes next.
@@ -7,18 +7,18 @@ git fetch origin
 currentbranch=$(git symbolic-ref --short -q HEAD)
 
 local="`git log --pretty=%H ...refs/heads/${currentbranch}^ | head -n 1`"
-origin="`git ls-remote origin -h refs/heads/${basebranch} |cut -f1`"
+origin="`git ls-remote origin -h refs/heads/${publishingbranch} |cut -f1`"
 dirtytree="$(git status --porcelain)"
 
 echo "your `tput setaf 3;tput smso` local `tput rmso;tput setaf 7` is at: `tput setaf 3;tput smso` ${currentbranch} ${local} `tput rmso;tput setaf 7`"
-echo "your `tput setaf 4;tput smso` origin `tput rmso;tput setaf 7` is at: `tput setaf 4;tput smso` ${basebranch} ${origin} `tput rmso;tput setaf 7`"
+echo "your `tput setaf 4;tput smso` origin `tput rmso;tput setaf 7` is at: `tput setaf 4;tput smso` ${publishingbranch} ${origin} `tput rmso;tput setaf 7`"
 
 if [[ "${dirtytree}" != "" ]]; then
   echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` your tree is DIRTY, please don't publish uncommited work. remember, you're about to publish a package version."
   exit 1
 fi
 
-if [[ "${basebranch}" != "${currentbranch}" ]]; then
+if [[ "${publishingbranch}" != "${currentbranch}" ]]; then
   echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` check if you're in your publishing branch. remember, you're about to publish a package version."
   exit 1
 fi
