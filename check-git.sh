5ae2846e18bc122adc84bcde7b9cf260a4d9f7e7
Angelo Reale <info@angeloreale.com>
2021-09-25 19:40:27 -0300

    better script

--- check-git.sh
+++ check-git.sh
@@ -8,20 +8,26 @@ currentbranch=$(git symbolic-ref --short -q HEAD)
 
 local="`git log --pretty=%H ...refs/heads/${currentbranch}^ | head -n 1`"
 origin="`git ls-remote origin -h refs/heads/${basebranch} |cut -f1`"
-dirtytree=$(git diff --stat)
+dirtytree="$(git status --porcelain)"
 
 echo "your `tput setaf 3;tput smso` local `tput rmso;tput setaf 7` is at: `tput setaf 3;tput smso` ${currentbranch} ${local} `tput rmso;tput setaf 7`"
 echo "your `tput setaf 4;tput smso` origin `tput rmso;tput setaf 7` is at: `tput setaf 4;tput smso` ${basebranch} ${origin} `tput rmso;tput setaf 7`"
 
 if [[ "${dirtytree}" != "" ]]; then
-  echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` your tree is DIRTY, please don't publish uncommited work."
+  echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` your tree is DIRTY, please don't publish uncommited work. remember, you're about to publish a package version."
   exit 1
 fi
 
-if [[ ("${origin}" != "${local}") || ("${basebranch}" != "${currentbranch}") ]]; then
-  echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` check if you're in main branch and you're up-to-date. remember, you're about to publish a package version."
+if [[ "${basebranch}" != "${currentbranch}" ]]; then
+  echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` check if you're in your publishing branch. remember, you're about to publish a package version."
   exit 1
-else
-  echo "`tput setaf 2;tput smso` √ GOOD `tput rmso;tput setaf 7`  to publish, doing it now..."
-  exit 0
 fi
+
+if [[ "${origin}" != "${local}" ]]; then
+  echo "`tput setaf 1;tput smso` FAIL `tput rmso;tput setaf 7` check if you're up-to-date with remote. remember, you're about to publish a package version."
+  exit 1
+fi
+
+echo "`tput setaf 2;tput smso` √ GOOD `tput rmso;tput setaf 7`  to publish, doing it now..."
+exit 0
+
